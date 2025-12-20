4fa1e754207399b6c5f8f29049940e04e70ac2ee
MehmetBahaDursun <baha.dursun123 @gmail.com>
2025-12-20 13:38:00 +0100

    # Vision part initial commit

---
+++ vision/Code/do_pe.py
@@ -0,0 +1,12 @@
+import open3d as o3d
+import copy
+import numpy as np
+from tqdm import tqdm
+import random
+
+def do_pose_estimation(scene_pointcloud, object_pointcloud):
+    print("YOU NEED TO IMPLEMENT THIS!")
+
+    
+
+    return np.identity(4)
